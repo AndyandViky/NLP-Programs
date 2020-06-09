17119421fb01319187c052a0aa6bae34d278a27a
andy <andy_viky@163.com>
2020-06-09 23:41:31 +0800

    init code

---
+++ config.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: andy
+@Contact: andy_viky@163.com
+@Github: https://github.com/AndyandViky
+@Csdn: https://blog.csdn.net/AndyViky
+@File: config.py
+@Time: 2020/6/9 下午10:57
+@Desc: config.py
+"""
+import os
+import torch
+
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Local directory of CypherCat API
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Local directory containing entire repo
+REPO_DIR = os.path.split(ROOT_DIR)[0]
+
+# Local directory for datas
+DATASETS_DIR = os.path.join(REPO_DIR, 'datas')
+
---
+++ week01_embeddings/embedding.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: andy
+@Contact: andy_viky@163.com
+@Github: https://github.com/AndyandViky
+@Csdn: https://blog.csdn.net/AndyViky
+@File: embedding.py
+@Time: 2020/6/9 下午10:55
+@Desc: embedding.py
+"""
+
